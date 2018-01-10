f6f10a5a727ef0eb0806afd3cdcf74647c64c5b3
haoling-m <steven.haoling@gmail.com>
2018-01-10 14:31:57 -0800

    remove space and comments

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 # Echo client program
 import socket
 
-#HOST = 'LOCALHOST'
 HOST = '192.168.66.187'
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,4 +9,4 @@ s.connect((HOST, PORT))
 s.send(bytes("hellooo", 'UTF-8'))
 data = s.recv(1024)
 s.close()
-print (data.decode('utf-8'))
+print(data.decode('utf-8'))
