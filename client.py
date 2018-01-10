08ad88ee49f8e56d8e7aa6f94b74fa22370edfec
haoling-m <steven.haoling@gmail.com>
2018-01-10 15:33:49 -0800

    remove a empty line

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 # Echo client program
 import socket
-
 HOST = '192.168.66.187'
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
